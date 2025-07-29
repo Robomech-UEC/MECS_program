0910f338236cb27d28063eac5569cb6090f8eb93
MAMMARU-class <144889491+MAMMARU-class@users.noreply.github.com>
2025-07-29 09:11:11 +0900

    writing custom-index disables search

--- _includes/lunr/custom-index.js
+++ _includes/lunr/custom-index.js
@@ -1,18 +1,18 @@
-lunr.tokenizer.separator = /[\s]+/; // 通常の空白分割は使わない（念のため）
+// lunr.tokenizer.separator = /[\s]+/; // 通常の空白分割は使わない（念のため）
 
-// load module for Japanese
-lunr.ja.load();
+// // load module for Japanese
+// lunr.ja.load();
 
-window.searchIndex = lunr(function () {
-  // use plugin for Japanese
-  this.use(lunr.ja);
+// window.searchIndex = lunr(function () {
+//   // use plugin for Japanese
+//   this.use(lunr.ja);
 
-  this.ref("id");
-  this.field("title", { boost: 15 });
-  this.field("content");
+//   this.ref("id");
+//   this.field("title", { boost: 15 });
+//   this.field("content");
 
-  docs.forEach(function (doc, index) {
-    doc.id = index;
-    this.add(doc);
-  }, this);
-});
+//   docs.forEach(function (doc, index) {
+//     doc.id = index;
+//     this.add(doc);
+//   }, this);
+// });
